796b1f507d17a1ecf1a4a15d793af4a99085d7c0
Ilhom <nzzmmvv@gmail.com>
2025-09-29 17:26:21 +0500

    get token

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 
 function Login() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("http://192.168.0.110:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username: login, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Login successful");
+        if (data.token) {
+          localStorage.setItem("token", data.token);
+        }
+      } else {
+        console.error("Login failed");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
   return (
     <div className="login">
       <span className="login__title">Digital gradebook</span>
-      <input type="text" placeholder="Login" />
-      <input type="text" placeholder="Password" />
-      <button>Log in</button>
+
+      <input
+        type="text"
+        placeholder="Login"
+        value={login}
+        onChange={(e) => setLogin(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Log in</button>
     </div>
   );
 }
---
+++ src/config.jsx
@@ -0,0 +1,3 @@
+const API = "http://192.168.0.110:3000/";
+
+export default API;
