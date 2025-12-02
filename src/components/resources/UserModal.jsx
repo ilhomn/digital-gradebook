1526dd4a42282d0c4815946bf016fc97686f0e19
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-12-03 03:26:19 +0500

    added password field

--- src/components/resources/UserModal.jsx
+++ src/components/resources/UserModal.jsx
@@ -12,6 +12,7 @@ const UserModal = ({ isOpen, onClose, onSubmit, userData }) => {
         koreanFirstName: "",
         koreanLastName: "",
         email: "",
+        password: "",
         role: "teacher",
     });
 
@@ -56,6 +57,7 @@ const UserModal = ({ isOpen, onClose, onSubmit, userData }) => {
                     <input type="text" name="koreanFirstName" placeholder="Korean First Name" value={form.koreanFirstName} onChange={(e) => setForm({ ...form, koreanFirstName: e.target.value })} required />
                     <input type="text" name="koreanLastName" placeholder="Korean Last Name" value={form.koreanLastName} onChange={(e) => setForm({ ...form, koreanLastName: e.target.value })} />
                     <input type="email" name="email" placeholder="Email" value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} />
+                    <input type="password" name="password" placeholder="Password" value={form.password} onChange={(e) => setForm({ ...form, password: e.target.value })} required />
 
                     {/* Custom dropdown for role */}
                     <div
